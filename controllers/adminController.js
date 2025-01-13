0bf4113c252a851e83cb61b36aa1328243ff85f5
Rafi5734 <kamrulhasun5734@gmail.com>
2025-01-13 16:52:44 +0600

    bcryptjs used

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config(); // Ensure this is at the top of your entry file
 
